5d2f2879a4198e3fa29ca09306a8219c51fbac77
FILIMON <f.rykov@bk.ru>
2024-04-16 21:26:40 +0300

    creating tasks

--- src/app/(platform)/(dashboard)/board/[boardId]/_component/list-item.tsx
+++ src/app/(platform)/(dashboard)/board/[boardId]/_component/list-item.tsx
@@ -5,6 +5,8 @@ import { ListWithCatds } from "@/types"
 import { ListHeader } from "./list-header";
 import { ElementRef, useRef, useState } from "react";
 import { CardForm } from "./card-form";
+import { cn } from "@/lib/utils";
+import { CardItem } from "./card-item";
 
 interface ListItemProps {
   data: ListWithCatds;
@@ -29,8 +31,6 @@ export const ListItem = ({
     })
   }
 
-
-
   return (
     <li className="shrink-0 h-full w-[272px] select-none">
       <div className="w-full rounded-md bg-[#f1f2f4] shadow-md pb-2">
@@ -38,6 +38,20 @@ export const ListItem = ({
           onAddCard={enableEditing}
           data={data}
         />
+        <ol
+          className={cn(
+            "mx-1 px-1 py-0.5 flex flex-col gap-y-2",
+            data.cards.length > 0 ? "mt-2" : "mt-0",
+          )}
+        >
+          {data.cards.map((card, index) => (
+            <CardItem
+              index={index}
+              key={card.id}
+              data={card}
+            />
+          ))}
+        </ol>
         <CardForm
           listId={data.id}
           ref={textareaRef}
